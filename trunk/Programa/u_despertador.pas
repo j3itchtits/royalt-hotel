5bccb0123aa07f85221c85e92464c20069e78176
igormateus0401@gmail.com <igormateus0401@gmail.com@c3e326b6-b83a-9f8e-4d28-6ed38345900e>
2012-06-07 14:02:33 +0000

    Att do despertador

--- trunk/Programa/u_despertador.pas
+++ trunk/Programa/u_despertador.pas
@@ -4,10 +4,31 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, u_dm, db;
+  Dialogs, u_dm, db, StdCtrls, DBCtrls, Buttons, PngSpeedButton, Grids, DBGrids,
+  ADODB, Mask;
 
 type
   Tf_despertador = class(TForm)
+    conexao: TADOConnection;
+    tb_despertador: TADOTable;
+    ds_despertador: TDataSource;
+    l_num: TLabel;
+    l_data: TLabel;
+    l_hora: TLabel;
+    grid_geral: TDBGrid;
+    l_gridGeral: TLabel;
+    grid_ativos: TDBGrid;
+    l_ativos: TLabel;
+    b_novo: TPngSpeedButton;
+    b_salvar: TPngSpeedButton;
+    b_cancelar: TPngSpeedButton;
+    b_excluir: TPngSpeedButton;
+    check_ativo: TDBCheckBox;
+    l_status: TLabel;
+    db_data: TDBEdit;
+    db_hora: TDBEdit;
+    db_num_quarto: TDBComboBox;
+    procedure db_dataChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,4 +42,3 @@ implementation
 
 {$R *.dfm}
 
-end.
