654df2ba4ab8b2fe944df4d813ff57267399da04
rodrigofelixrodrigo@gmail.com <rodrigofelixrodrigo@gmail.com@c3e326b6-b83a-9f8e-4d28-6ed38345900e>
2012-06-18 00:05:21 +0000

    Formulário de configuração de senha

--- trunk/Programa/u_cadastro_senha.pas
+++ trunk/Programa/u_cadastro_senha.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, Buttons, PngSpeedButton, StdCtrls, u_dm;
 
 type
   Tf_cadastro_senha = class(TForm)
+    t_senha_atual: TEdit;
+    t_senha_nova: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    b_salvar: TPngSpeedButton;
+    procedure b_salvarClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -16,9 +23,45 @@ type
 
 var
   f_cadastro_senha: Tf_cadastro_senha;
+  senha : string;
 
 implementation
 
 {$R *.dfm}
+
+procedure Tf_cadastro_senha.b_salvarClick(Sender: TObject);
+begin
+  with dm.q_senha do
+  begin
+  Close;
+  SQL.Clear;
+  sql.Add('select senha from senha where senha = '''+t_senha_atual.text+'''');
+  Open;
+  if RecordCount = 0 then
+    begin
+    showmessage('Senha atual está errada!');
+    exit
+    end
+    else
+    begin
+    Close;
+    SQL.Clear;
+    sql.Add('update senha set senha = '''+t_senha_nova.text+''' where senha = '''+t_senha_atual.text+''' ');
+    ExecSQL;
+    showmessage('Senha alterada com sucesso!');
+    end;
+
+  end;
+t_senha_nova.text := '';
+t_senha_atual.text := '';
+
+end;
+
+
+procedure Tf_cadastro_senha.FormShow(Sender: TObject);
+begin
+t_senha_nova.text := '';
+t_senha_atual.text := '';
+end;
 
 end.
